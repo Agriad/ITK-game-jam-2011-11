473dbc7d9c936b61598153693b1773c80866ace6
Agriad <agriad1@yahoo.com>
2021-11-05 23:05:24 +0100

    Add square animation on click

--- javascript.js
+++ javascript.js
@@ -3,8 +3,10 @@ var ctx = canvas.getContext("2d");
 
 var xMousePosition = 0;
 var yMousePosition = 0;
+var clickAction = false;
 
 canvas.addEventListener("mousemove", findCursor, false);
+canvas.addEventListener("click", clickHandler, false);
 
 function drawBackground() {
     ctx.beginPath();
@@ -24,14 +26,46 @@ function drawCircle(x, y) {
     ctx.closePath();
 }
 
+function drawSquare(x, y) {
+    ctx.beginPath();
+    ctx.rect(x, y, 0, 50, 50);
+    ctx.fillStyle = "#1F85DE";
+    ctx.fill();
+    ctx.stroke();
+    ctx.closePath();
+}
+
+function drawSquareAnimation() {
+    if (clickAction) {
+        for (let index = 0; index < 10; index++) {
+            drawBackground();
+            // drawCircle();
+            drawSquare(xMousePosition, yMousePosition + index);
+        }
+    
+        for (let index = 10; index > 0; index++) {
+            drawBackground();
+            // drawCircle();
+            drawSquare(xMousePosition, yMousePosition + index);
+        }
+        
+        clickAction = false;
+    }
+}
+
 function findCursor(event) {
     xMousePosition = event.pageX;
     yMousePosition = event.pageY;
 }
 
+function clickHandler(event) {
+    clickAction = true;
+}
+
 function test() {
     drawBackground();
     drawCircle(xMousePosition, yMousePosition);
+    drawSquareAnimation();
 }
 
 setInterval(test, 100);
